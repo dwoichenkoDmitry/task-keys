9450414f4ca22df461fe44b81a40a14d38947538
dwoichenkoDmitry <66391940+dwoichenkoDmitry@users.noreply.github.com>
2022-05-26 03:51:59 +0500

    task-keys-changes

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -2,28 +2,16 @@ import { IItem } from './index';
 import KeysItem from './KeysItem';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
+    const sortingMap = props.initialData
+        .map((item: IItem) => {
+            return (
+                <li key={item.id}>
+                    <KeysItem name={item.name} />
+                </li>
+            );
+        })
+        .sort();
     return (
-        <div>
-            {props.sorting === 'ASC'
-                ? props.initialData
-                      .map((item: IItem) => {
-                          return (
-                              <li key={item.id}>
-                                  <KeysItem name={item.name} />
-                              </li>
-                          );
-                      })
-                      .sort()
-                : props.initialData
-                      .map((item: IItem) => {
-                          return (
-                              <li key={item.id}>
-                                  <KeysItem name={item.name} />
-                              </li>
-                          );
-                      })
-                      .sort()
-                      .reverse()}
-        </div>
+        <div>{props.sorting === 'ASC' ? sortingMap : sortingMap.reverse()}</div>
     );
 }
