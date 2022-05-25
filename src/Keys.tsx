8f6c8cf6f3dbbecc6e5b779f54fe3e9abb8981bd
dwoichenkoDmitry <66391940+dwoichenkoDmitry@users.noreply.github.com>
2022-05-26 00:42:57 +0500

    task-keys

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,29 @@
 import { IItem } from './index';
+import KeysItem from './KeysItem';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    return (
+        <div>
+            {props.sorting === 'ASC'
+                ? props.initialData
+                      .map((item: IItem) => {
+                          return (
+                              <li key={item.id}>
+                                  <KeysItem name={item.name} />
+                              </li>
+                          );
+                      })
+                      .sort()
+                : props.initialData
+                      .map((item: IItem) => {
+                          return (
+                              <li key={item.id}>
+                                  <KeysItem name={item.name} />
+                              </li>
+                          );
+                      })
+                      .sort()
+                      .reverse()}
+        </div>
+    );
 }
